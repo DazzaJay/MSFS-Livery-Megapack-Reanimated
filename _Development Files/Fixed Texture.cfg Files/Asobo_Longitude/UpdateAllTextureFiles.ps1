74f256bc3d810ba6527638caab8507630fed3928
Daniel Fitzgerald <dazzajay@potholestudios.com>
2023-07-07 23:09:11 +1000

    Longitude Pink Texture Fix Applied

---
+++ _Development Files/Fixed Texture.cfg Files/Asobo_Longitude/UpdateAllTextureFiles.ps1
@@ -0,0 +1,7 @@
+$original = "R:\GitHub\MSFS2020-livery-megapack\Asobo_Longitude" # The path to the directory containing the files you want to replace
+$replacement = "R:\GitHub\MSFS2020-livery-megapack\_Development Files\Fixed Texture.cfg Files\Asobo_Longitude" # The path to the directory containing the replacement files
+
+Get-ChildItem $original -Filter "texture.cfg" -Recurse | ForEach-Object {
+    $destination = Split-Path $_.FullName -Parent
+    Copy-Item (Join-Path $replacement "texture.cfg") -Destination $destination -force
+}
